d68a98e45877bc22291a532871d1b588f674c23f
FakeChanelini <blakespurdle@gmail.com>
2023-12-07 18:48:37 +1300

    ascii art function added

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import openai
 import discord
 from dotenv import load_dotenv
 from openai import Completion
+import random
+from arts import ascii_arts
 
 load_dotenv()
 
@@ -34,13 +36,82 @@ async def on_message(message: discord.Message):
     """
     Listen to message event
     """
-    # ignore messages from the bot itself and messages that don't start with the prefix we have set
-    if message.author == client.user or not not message.content.startswith(PREFIX):
+    # ignore messages from the bot itself
+    if message.author == client.user:
         return 
+
+    # check if the bot was mentioned
+    if client.user in message.mentions:
+        # get the message content, remove the mention
+        text = message.content.replace('@RealBot', '').strip()
+        # if there's any text left, treat it as a command
+        if text:
+            await chat(message, text)
+        else:
+            await message.channel.send("You mentioned me. How can I assist you?")
+        return
 
     # listen to any messages that start with '>>ask'
     if message.content.startswith(">>ask"):
         await chat(message)
+
+    # listen to any messages that start with '>>art'
+    if message.content.startswith(">>art"):
+        await art(message)
+
+async def chat(message: discord.Message, text=None):
+    if text is None:
+        try:
+            # get the prompt from the messsage by spliting the command
+            command, text = message.content.split(" ", maxsplit=1)
+
+
+        except ValueError:
+            await message.channel.send("Please provide a question using the >>ask command")
+            return
+
+    # get reponse from openai's text-davinci-003 aka GPT-3
+    # You can play around with the filter to get a better result
+    # Visit https://beta.openai.com/docs/api-reference/completions/create for more info
+    response = Completion.create(
+        engine="text-davinci-003",
+        prompt=text,
+        temperature=0.8,
+        max_tokens=512,
+        top_p=1,
+        logprobs=10,
+    )
+
+    # Extract the response from the API response
+    response_text = response["choices"][0]["text"]
+
+    await message.channel.send(response_text)
+
+
+async def art(message: discord.Message):
+    # split the message content into command and art_type
+    split_message = message.content.split(" ", maxsplit=1)
+
+    # check if art_type is provided
+    if len(split_message) < 2:
+        await message.channel.send("Please provide the type of art after the >>art command")
+        return
+
+    command, art_type = split_message
+
+    # select a random ASCII art from the list
+    art = random.choice(ascii_arts)
+
+    await message.channel.send(f"Here is your {art_type} art:\n```\n{art}\n```")
+
+
+# start the bot
+client.run(TOKEN)
+
+
+
+
+
 
 
 async def chat(message: discord.Message):
