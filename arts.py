740bf8da516e011f8e4847f96b9828447b17a0b0
FakeChanelini <blakespurdle@gmail.com>
2023-12-10 22:15:03 +1300

    fix main code

--- arts.py
+++ arts.py
@@ -1,3 +1,7 @@
+import random
+import discord
+
+
 ascii_arts = [
     """
     ^_^
@@ -210,3 +214,18 @@ ascii_arts = [
     # add more ASCII arts here...
 ]
 
+async def art(message: discord.Message):
+    # split the message content into command and art_type
+    split_message = message.content.split(" ", maxsplit=1)
+
+    # check if art_type is provided
+    if len(split_message) < 2:
+        await message.channel.send("Please provide the type of art after the >>art command")
+        return
+
+    command, art_type = split_message
+
+    # select a random ASCII art from the list
+    art = random.choice(ascii_arts)
+
+    await message.channel.send(f"Here is your {art_type} art:\n```\n{art}\n```")
--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from openai import Completion
 import random
 import asyncio
-from arts import ascii_arts
+from arts import ascii_arts, art
 import io
 import base64
 from api_img import call_api_and_get_image_data
@@ -34,7 +34,7 @@ openai.api_key = aiKey
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='>>', intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('>>'), intents=intents)
 
 
 @bot.event
@@ -45,86 +45,7 @@ async def on_ready():
     print(f"We have logged in as {bot.user}")
     
 
-PREFIX = "!"
-
-@bot.event
-async def on_message(message: discord.Message):
-    """
-    Listen to message event
-    """
-    # ignore messages from the bot itself
-    if message.author == bot.user:
-        return 
-  
-
-    # check if the bot was mentioned
-    if bot.user in message.mentions:
-        # get the message content, remove the mention
-        text = message.content.replace('@RealBot', '').strip()
-        # if there's any text left, treat it as a command
-        if text:
-            await chat(message, text)
-        else:
-            await message.channel.send("You mentioned me. How can I assist you?")
-        return
-
-    # listen to any messages that start with '>>ask'
-    if message.content.startswith(">>ask"):
-        await chat(message)
-
-    # listen to any messages that start with '>>art'
-    if message.content.startswith(">>art"):
-        await art(message)
-
-    if message.content.startswith('>>charge'):
-        ctx = await bot.get_context(message)
-        print(f"charging")
-        await preload_data(ctx)
-        print(f"Server data loaded")
-
-    await bot.process_commands(message)
-
-async def chat(message: discord.Message, text=None):
-    if text is None:
-        try:
-            # get the prompt from the messsage by spliting the command
-            command, text = message.content.split(" ", maxsplit=1)
-
-
-        except ValueError:
-            await message.channel.send("Please provide a question using the >>ask command")
-            return
-
-    response = Completion.create(
-        engine="text-davinci-003",
-        prompt=text,
-        temperature=0.8,
-        max_tokens=512,
-        top_p=1,
-        logprobs=10,
-    )
-
-    # Extract the response from the API response
-    response_text = response["choices"][0]["text"]
-
-    await message.channel.send(response_text)
-
-
-async def art(message: discord.Message):
-    # split the message content into command and art_type
-    split_message = message.content.split(" ", maxsplit=1)
-
-    # check if art_type is provided
-    if len(split_message) < 2:
-        await message.channel.send("Please provide the type of art after the >>art command")
-        return
-
-    command, art_type = split_message
-
-    # select a random ASCII art from the list
-    art = random.choice(ascii_arts)
-
-    await message.channel.send(f"Here is your {art_type} art:\n```\n{art}\n```")
+PREFIX = ">>"
 
 async def api_art(message: discord.Message):
     # split the message content into command and art_type
@@ -183,10 +104,16 @@ async def on_message(message: discord.Message):
         text = message.content.replace('@RealBot', '').strip()
         # if there's any text left, treat it as a command
         if text:
-            await chat(message, text)
+            await chat(message)
         else:
             await message.channel.send("You mentioned me. How can I assist you?")
         return
+    
+    if message.content.startswith(">>charge"):
+        print(f"charging")
+        ctx = await bot.get_context(message)
+        await preload_data(ctx)
+        print(f"Server data loaded")
 
     # listen to any messages that start with '>>ask'
     if message.content.startswith(">>ask"):
@@ -248,7 +175,7 @@ async def chat(message: discord.Message):
         # get the prompt from the messsage by spliting the command
         command, text = message.content.split(" ", maxsplit=1)
     except ValueError:
-        await message.channel.send("Please provide a question using the >>ask command")
+        await message.channel.send("You mentioned me. How can I assist you? Try using: >>art, >>apiart, >>stats, >>charge, or >>words")
         return
 
     response = Completion.create(
